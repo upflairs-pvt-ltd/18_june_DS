9750a658e557faa2fee3362a2d4646020e453b69
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-19 14:41:41 +0530

    19 june

---
+++ first.py
@@ -0,0 +1,20 @@
+# print("Hello world")
+
+
+# num1 = 25.02
+# print(num1)
+# print(type(num1)) 
+
+
+# boolean bool 
+
+
+var1 = True 
+var2 = False 
+print(var1)
+print(var2) 
+print(type(var1))
+print(type(var2))
+
+
+# int , float , bool
