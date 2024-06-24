5f61094c78be1e5fae3900ecc98801434b6ab95a
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-24 14:47:07 +0530

    '24june'

---
+++ day6.py
@@ -0,0 +1,78 @@
+# <<<<<<   LOOP   >>>>>>>>>>>>
+
+
+# for i in range(5,11):
+#     print("Hello world! ",i)
+
+# print("for loop end")
+
+
+# without using loop 
+# ls = [25,41,63,96,85,74,45,8,62,12]
+
+# if 85 in ls:
+#     print('present')
+# else:
+#     print('not present')
+
+#  item <= 50
+# ls = [25,41,63,96,85,74,45,8,62,12,41,32,856,96,97,94]
+# n = len(ls)   # 10 
+
+# for i in range(n):  # 0 --  9 
+#     if ls[i] <= 50:
+#         print(ls[i])
+         
+
+
+
+
+# ls[0] ==>  25
+# ls[1] ==>  41
+# ls[2] ==>  63
+# ls[3] ==>  96
+# ls[5] ==>  85
+# ls[6] ==>  74
+# ls[7] ==>  45
+
+
+
+# ls = [25,41,63,96,85,74,45,8,62,12,41,32,856,96,97,94]
+
+# for item in ls:
+#     if item <= 50:
+#         print(item)
+
+
+
+# i = 0   # starting 
+# while i <= 10:   # Stoping   
+#     print(' WELCOME ')
+#     i += 1     # i = i + 1 
+
+
+# condition = True
+# while condition:   #False 
+#     queit = input("do you want to quiet it ? press Y/y : ")
+#     if queit =="Y" or queit == "y":
+#         condition = False 
+#     print(" welcome ")
+
+
+
+# ls = [25,41,63,96,85,74,45,8,62,12,41,32,856,96,97,94]
+# even numbers loop (for, while,both)
+
+# for item in ls:
+#     if item % 2 == 0: # even number find out   
+#         print(item)
+
+# 25 % 2 == 0:
+
+
+
+ls = [25,41,63,96,85,74,45,8,62,12,41,32,856,96,97,94]
+
+# average_find_out == Ls
+
+# find out the total sum of each and every item  
